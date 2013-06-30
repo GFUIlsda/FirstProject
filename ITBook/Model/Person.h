9d36b1369600da9bae15b128a833998d5a953a64
SkyLORD <skylord05@ukr.net>
2013-06-30 13:08:30 -0700

    Update Person.h

--- ITBook/Model/Person.h
+++ ITBook/Model/Person.h
@@ -6,39 +6,39 @@
 //  Copyright (c) 2013 MacBook Air. All rights reserved.
 //
 
+// <challenge the directive #import, shall connect a type library that describes the use of COM interfaces>
 #import <Foundation/Foundation.h>
 
-@interface Person : NSObject
-// объявление переменных в проекте:
+// <creating a scripted class "Person" and ad parents class name "NSObject", you specify the class interface in an @interface...@end>
+@interface Person: NSObject
+
+// declaration of variables in the project:
 {
-  NSString *_firstName;
+	NSString *_firstName;
 	NSString *_lastName;
 }
 
-// НОВОЕ!!!
+// <given the following class declaration>
 @property (nonatomic, copy) NSString *firstName;
 @property (nonatomic, copy) NSString *lastName;
-// Закрытое НОВОЕ!!!
 
-// объявление методов обработки в проекте (не понятно почему так дублируем каждую новую строчку):
+// ad processing methods in the project:
 - (id)initWithFirstName:(NSString *)aFirstName;
 - (id)initWithFirstName:(NSString *)aFirstName lastName:(NSString *)aLastName;
 
-
-// не понятно для чего это:
+// <description of pointers to type of information>
 - (void)setFirstName:(NSString *)aFirstName;
 - (NSString *)firstName;
 - (void)setLastName:(NSString *)aLastName;
 - (NSString *)lastName;
 
-// --
+// <ad NSDictionary for need a convenient and efficient way to retrieve data associated with an arbitrary key>
 - (id)initWithDictionaryRepresentation:(NSDictionary *)aDict;
 - (NSDictionary *)dictionaryRepresentation;
-// --
 
 - (NSString *)stringRepresentation;
 
-// сортировка вводимых данных
+// sorting the input data:
 + (NSArray *)booksSortByFirstNameWithSet:(NSSet *)aPersons;
 + (NSArray *)booksSortByLastNameWithSet:(NSSet *)aPersons;
 
